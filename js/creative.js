f8fe087963bcde574d916ddc7316ecdbe4784cf9
Ben West <Xodarap@users.noreply.github.com>
2020-06-19 13:21:58 -0700

    xhr working

--- js/creative.js
+++ js/creative.js
@@ -45,4 +45,42 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
+    $('#ratingForm').submit(function(event){
+        event.preventDefault();
+        $('#loaderHolder').show();
+        var data = new FormData();
+        data.append('image', $('#photo')[0].files[0]);
+        var url = 'https://tiktokhot.azurewebsites.net/api/classify?code=WKdDWUMfuFd8k/YKntvMhQC1uOTxpxGuEEOuEZ36XaRg0FidnazwNg==';
+        $.ajax({url: url, 
+            data: data,
+            type: 'POST',
+            contentType: false,
+            processData: false,
+            success: function(result){
+                $('#loaderHolder').hide();
+                $('#resultsHolder').show();
+                $('#resultText').text(
+                    'The algorithm gave this picture a score of ' +
+                    Math.round(result['score'] * 100)/100 +
+                    ' out of 5, which is at the '
+                    + result['percentile'] * 100 +
+                    'th percentile.'
+                )
+            }
+        })
+    });
+
+    $('#photo').on('change', function(){
+        var reader = new FileReader();
+        reader.onloadend = function(event) {
+            $('#preview').attr('src', event.target.result);
+            //.css({'background-image':             'url(' + event.target.result + ')'});
+        }
+        var files = $('#photo')[0].files;
+        if(files && files.length > 0){
+            $('#preview').show();
+            reader.readAsDataURL(files[0]);
+        }
+    })
+
 })(jQuery); // End of use strict
