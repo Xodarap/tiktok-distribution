2f6da94ad49d904e56b1a0a692ba73e1447636a3
Ben West <Xodarap@users.noreply.github.com>
2020-06-20 15:41:03 -0700

    resize images

--- _site/js/creative.js
+++ _site/js/creative.js
@@ -99,6 +99,42 @@
     $('#photo').on('blur', validate);
     $('#username').on('blur', validate);
 
+    var resizeImage = function(file, cb) {
+        var img = document.createElement("img");
+        var reader = new FileReader();  
+        reader.onload = function(e) {
+            img.src = e.target.result;
+            var canvas = document.createElement('canvas');
+            canvas.style.display = 'none';
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+
+            var MAX_WIDTH = 224;
+            var MAX_HEIGHT = 224;
+            var width = img.width;
+            var height = img.height;
+
+            if (width > height) {
+                if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width;
+                    width = MAX_WIDTH;
+                }
+            } else {
+                if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height;
+                    height = MAX_HEIGHT;
+                }
+            }
+            canvas.width = width;
+            canvas.height = height;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0, width, height);
+
+            cb(canvas);
+        }
+        reader.readAsDataURL(file);
+    };
+
     $('#ratingForm').submit(function(event){
         event.preventDefault();
         if(!validate()) {
@@ -106,28 +142,34 @@
         }
 
         $('#loaderHolder').show();
-        var data = new FormData();
-        data.append('image', $('#photo')[0].files[0]);
-        var username = $('#username').val();
-        var url = 'https://tiktokhot.azurewebsites.net/api/classify?code=WKdDWUMfuFd8k/YKntvMhQC1uOTxpxGuEEOuEZ36XaRg0FidnazwNg==' +
-        '&username=' + username;
-        $.ajax({url: url, 
-            data: data,
-            type: 'POST',
-            contentType: false,
-            processData: false,
-            success: function(result){
-                $('#loaderHolder').hide();
-                $('#resultsHolder').show();
-                $('#resultText').text(
-                    'The algorithm gave this picture a score of ' +
-                    Math.round(result['score'] * 100)/100 +
-                    ' out of 5, which is at the '
-                    + Math.round(result['percentile'] * 100) +
-                    'th percentile.'
-                )
-            }
-        })
+        var file = $('#photo')[0].files[0];
+        var after = function(canvas) {
+            canvas.toBlob(function(blob){
+                var data = new FormData();
+                data.append('image', blob, file.name);
+                var username = $('#username').val();
+                var url = 'https://tiktokhot.azurewebsites.net/api/classify?code=WKdDWUMfuFd8k/YKntvMhQC1uOTxpxGuEEOuEZ36XaRg0FidnazwNg==' +
+                '&username=' + username;
+                $.ajax({url: url, 
+                    data: data,
+                    type: 'POST',
+                    contentType: false,
+                    processData: false,
+                    success: function(result){
+                        $('#loaderHolder').hide();
+                        $('#resultsHolder').show();
+                        $('#resultText').text(
+                            'The algorithm gave this picture a score of ' +
+                            Math.round(result['score'] * 100)/100 +
+                            ' out of 5, which is at the '
+                            + Math.round(result['percentile'] * 100) +
+                            'th percentile.'
+                        )
+                    }           
+                })
+            });
+        };
+        resizeImage(file, after);
     });
 
 })(jQuery); // End of use strict
