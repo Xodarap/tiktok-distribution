bd457306322d4142bf3f42f3942df3bcfd1ef151
Ben West <Xodarap@users.noreply.github.com>
2020-07-05 09:23:47 -0700

    init

--- js/creative.js
+++ js/creative.js
@@ -45,131 +45,81 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
-    var validateImage = function() {
-        var files = $('#photo')[0].files;
-        if(!files || files.length == 0){            
-            $('#photoError').show();
-            return false;
-        }
-        if(files[0].type.slice(0,6) !== 'image/'){
-            $('#photoError').show();
-            return false;        
-        }
-        $('#photoError').hide();
-        return true;
-    }
-    $('#photo').on('change', function(){        
-        if(!validateImage()){
-            return;
-        }
+   // Custom
 
-        var reader = new FileReader();
-        reader.onloadend = function(event) {
-            $('#preview').attr('src', event.target.result);
-
-            var img = new Image();
-            $(img).on('load', function(imgEvent) {
-                var ratio = img.naturalHeight / img.naturalWidth;
-                if(ratio < 0.8 || ratio > 1.3) {
-                    $('#dimensionError').show();
-                } else {
-                    $('#dimensionError').hide();
-                }
-            });
-            img.src = event.target.result;
-        }
-        var files = $('#photo')[0].files;
-        if(files && files.length > 0){
-            $('#preview').show();
-            reader.readAsDataURL(files[0]);
-        } 
-    })
-
-    var validate = function () {
-        var username = $('#username').val();
-        if(username.match(/^@?\S+$/) === null) {
-            $('#userError').show();
-            return false;
-        }        
-        $('#userError').hide();
-
-        return validateImage();
-    }
-    
-    $('#photo').on('blur', validate);
-    $('#username').on('blur', validate);
-
-    var resizeImage = function(file, cb) {
-        var img = document.createElement("img");
-        var reader = new FileReader();  
-        reader.onload = function(e) {
-            img.src = e.target.result;
-            var canvas = document.createElement('canvas');
-            canvas.style.display = 'none';
-            var ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0);
-
-            var MAX_WIDTH = 224;
-            var MAX_HEIGHT = 224;
-            var width = img.width;
-            var height = img.height;
-
-            if (width > height) {
-                if (width > MAX_WIDTH) {
-                    height *= MAX_WIDTH / width;
-                    width = MAX_WIDTH;
-                }
-            } else {
-                if (height > MAX_HEIGHT) {
-                    width *= MAX_HEIGHT / height;
-                    height = MAX_HEIGHT;
-                }
-            }
-            canvas.width = width;
-            canvas.height = height;
-            var ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0, width, height);
-
-            cb(canvas);
-        }
-        reader.readAsDataURL(file);
+    var getPercentile = function(data, followers) {
+        var ok = data.filter(x => x[1] <= followers);
+        return ok[ok.length - 1];
     };
 
-    $('#ratingForm').submit(function(event){
-        event.preventDefault();
-        if(!validate()) {
-            return;
+    var describePercentile = function(percentile) {
+        switch(percentile % 10){
+            case 1: return String(percentile) + 'st percentile';
+            case 2: return String(percentile) + 'nd percentile';
+            case 3: return String(percentile) + 'rd percentile';
+            default: return String(percentile) + 'th percentile';
         }
+    }
 
-        $('#loaderHolder').show();
-        var file = $('#photo')[0].files[0];
-        var after = function(canvas) {
-            canvas.toBlob(function(blob){
-                var data = new FormData();
-                data.append('image', blob, file.name);
-                var username = $('#username').val();
-                var url = 'https://tiktokhot.azurewebsites.net/api/classify?code=WKdDWUMfuFd8k/YKntvMhQC1uOTxpxGuEEOuEZ36XaRg0FidnazwNg==' +
-                '&username=' + username;
-                $.ajax({url: url, 
-                    data: data,
-                    type: 'POST',
-                    contentType: false,
-                    processData: false,
-                    success: function(result){
-                        $('#loaderHolder').hide();
-                        $('#resultsHolder').show();
-                        $('#resultText').text(
-                            'The algorithm gave this picture a score of ' +
-                            Math.round(result['score'] * 100)/100 +
-                            ' out of 5, which is at the '
-                            + Math.round(result['percentile'] * 100) +
-                            'th percentile.'
-                        )
-                    }           
-                })
-            });
-        };
-        resizeImage(file, after);
-    });
+    var updateChart = function(followers = undefined) {        
+          var percentiles = [[1, 0], [2, 0], [3, 1], [4, 1], [5, 1], [6, 2], [7, 2], [8, 3], [9, 3], [10, 3], [11, 4], [12, 5], [13, 5], [14, 6], [15, 6], [16, 7], [17, 7], [18, 8], [19, 8], [20, 9], [21, 9], [22, 10], [23, 11], [24, 12], [25, 14], [26, 15], [27, 16], [28, 18], [29, 19], [30, 20], [31, 22], [32, 23], [33, 25], [34, 26], [35, 30], [36, 31], [37, 32], [38, 34], [39, 37], [40, 41], [41, 44], [42, 47], [43, 48], [44, 53], [45, 55], [46, 58], [47, 62], [48, 69], [49, 75], [50, 79], [51, 84], [52, 88], [53, 96], [54, 107], [55, 126], [56, 138], [57, 165], [58, 171], [59, 189], [60, 200], [61, 216], [62, 228], [63, 238], [64, 264], [65, 287], [66, 309], [67, 354], [68, 382], [69, 413], [70, 458], [71, 553], [72, 579], [73, 625], [74, 748], [75, 862], [76, 931], [77, 1045], [78, 1105], [79, 1230], [80, 1493], [81, 1744], [82, 2046], [83, 2579], [84, 4306], [85, 5797], [86, 8277], [87, 11709], [88, 16221], [89, 18850], [90, 33510], [91, 47407], [92, 61181], [93, 81370], [94, 108864], [95, 133196], [96, 183623], [97, 318487], [98, 397433], [99, 479964]];
+          var line = {
+            x: percentiles.map(x => x[0]),
+            y: percentiles.map(x => x[1]),
+            fill: 'tozeroy',
+            type: 'scatter'
+          };
+          var data = [line];
+          var annotations = [];
+
+          if(followers !== undefined) {
+            var p = getPercentile(percentiles, followers);
+            annotations = [{
+                x: p[0],
+                y: Math.log10(p[1]),
+                xref: 'x',
+                yref: 'y',
+                text: describePercentile(p[0]),
+                showarrow: true,
+                arrowhead: 7,
+                ax: 0,
+                ay: -40
+            }]
+          }
+          var layout = {
+              showlegend: false,
+              margin: {
+                  l: 0,
+                  r: 0,
+                  t: 0,
+                  b: 0,
+                  pad: 0
+              },
+              height: 250,
+              width: 275,
+              xaxis: {
+                  visible: true,
+                  title: 'Percentile',
+                  automargin: true
+              },
+              yaxis: {
+                  visible: true,
+                  title: 'Follower Count',
+                  type: 'log',
+                  automargin: true
+              },
+              annotations: annotations
+          }
+          var config = {
+              displayModeBar: false,
+              responsive: true
+            }
+          
+          Plotly.newPlot('plotHolder', data, layout, config);
+    };
+    updateChart();
+    $('#followerCount').on('blur', function(){
+        updateChart($(this).val())
+    })
 
 })(jQuery); // End of use strict
